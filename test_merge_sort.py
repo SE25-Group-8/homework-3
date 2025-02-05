e33d3cabe0a3add674fedf7b921f0c948e7b01ad
atharvapatil22 <atharva.22.patil@gmail.com>
2025-02-04 19:05:28 -0500

    renamed tests

--- test_merge_sort.py
+++ test_merge_sort.py
@@ -1,14 +1,14 @@
 import pytest
 from hw2_debugging import merge_sort
 
-def test_merge_sort_empty():
+def test_7_empty_arr():
     """Test merge sort with an empty array"""
     assert merge_sort([]) == []
 
-def test_merge_sort_floats():
+def test_8_floats():
     """Test merge sort with an array containing floats"""
     assert merge_sort([2.5, 3.1, 1.2, 5.0]) == [1.2, 2.5, 3.1, 5.0]
 
-def test_merge_sort_reverse_sorted():
+def test_9_reverse():
     """Test merge sort with an array in reverse order"""
     assert merge_sort([5, 4, 3, 2, 1]) == [1, 2, 3, 4, 5]
